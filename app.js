4d2d01030d17da9ddbd6ddf1a6d43a84835ef391
tonycimaglia <tonycimaglia@gmail.com>
2018-01-23 09:02:03 -0500

    trying to build out api. added users controller and required it in app.js.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const UsersController = require('./controllers/users')
 
 var app = express();
 
@@ -41,6 +42,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/users', UsersController)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
