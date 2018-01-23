01d984fec9c1134ea003557d2b190ab7cfa6d77e
tonycimaglia <tonycimaglia@gmail.com>
2018-01-22 19:53:38 -0500

    testing out the publicspaces seed

--- db/seeds.js
+++ db/seeds.js
@@ -39,33 +39,6 @@ PublicSpace.remove({})
     .catch((error) => {
         console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
         console.log(error)
-    })
-
-// Delete all users, then add some fake ones
-User.remove({})
-    .then(() => {
-        const seedUser1 = new User({
-            userName: 'Eva',
-            email: 'evaCimaglia@gmail.com',
-            privateSpaces: []
-        })
-
-        const seedPrivateSpace1 = new PrivateSpace({
-            neighborhood: 'Candler Park',
-            description: 'Private Parking Space in Euclid Court Apartments',
-            location: '1335 Euclid Ave. Atlanta, GA 30307',
-            hours: 'M-F, 8:00am-6:00pm',
-            availability: true,
-            public: false
-        })
-
-        seedUser1.privateSpaces.push(seedPrivateSpace1)
-
-        return seedUser1.save()
-
-    }).catch((error) => {
-        console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
-        console.log(error)
     }).then(() => {
         mongoose.connection.close()
         console.log(`
@@ -74,3 +47,37 @@ User.remove({})
       Disconnected from MongoDB
     `)
     })
+
+// Delete all users, then add some fake ones
+// User.remove({})
+//     .then(() => {
+//         const seedUser1 = new User({
+//             userName: 'Eva',
+//             email: 'evaCimaglia@gmail.com',
+//             privateSpaces: []
+//         })
+
+//         const seedPrivateSpace1 = new PrivateSpace({
+//             neighborhood: 'Candler Park',
+//             description: 'Private Parking Space in Euclid Court Apartments',
+//             location: '1335 Euclid Ave. Atlanta, GA 30307',
+//             hours: 'M-F, 8:00am-6:00pm',
+//             availability: true,
+//             public: false
+//         })
+
+//         seedUser1.privateSpaces.push(seedPrivateSpace1)
+
+//         return seedUser1.save()
+
+//     }).catch((error) => {
+//         console.log('!!!!! ERROR SAVING SEEDED DATA !!!!!')
+//         console.log(error)
+//     }).then(() => {
+//         mongoose.connection.close()
+//         console.log(`
+//       Finished seeding database...
+
+//       Disconnected from MongoDB
+//     `)
+//     })
