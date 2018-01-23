bcf3ec9cf2f6d921a3344d0ebbab5a3ac34a23c9
tonycimaglia <tonycimaglia@gmail.com>
2018-01-23 12:45:26 -0500

    Users are rendering in the show all users page.

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
-// import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
+import axios from 'axios'
 
 class UsersList extends Component {
+    state = {
+        users: []
+    }
+
+    async componentWillMount() {
+        const response = await axios.get('/api/users')
+        this.setState({ users: response.data })
+    }
+
     render() {
         return (
             <div>
-                <h1>This is the list of users:</h1>
+                <h1>This is the list of Existing users:</h1>
+                {this.state.users.map(user => {
+                    return (<Link to={`/user/${user._id}`}>{user.userName}</Link>)
+                })}
             </div>
         )
     }
