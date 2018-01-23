312863ca70cb2a972ce7182e8afc501ec9c75dfd
tonycimaglia <tonycimaglia@gmail.com>
2018-01-23 13:18:12 -0500

    added a post route in the users controller.

--- controllers/users.js
+++ controllers/users.js
@@ -9,5 +9,15 @@ router.get('/', (req, res) => {
   .catch((err) => console.log(err))
 })
 
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await User.create({}) // create a new Idea, let Mongoose give the default values
+    res.json(newUser) // Send this new idea back to the client
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500) // If there is any error, tell the client something went wrong on the server
+  }
+})
+
 module.exports = router
 
