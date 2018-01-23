2c20f49d8e23ab5577c91e8c91ec329fb0bf4c72
tonycimaglia <tonycimaglia@gmail.com>
2018-01-23 10:07:37 -0500

    trying to build out Neighborhoods component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import NeighborhoodsPage from './components/NeighborhoodsPage'
+// import User from './components/User'
+// import UsersList from './components/UsersList'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={NeighborhoodsPage}/>
+            {/* <Route path="/users" component={UsersList}/>
+            <Route path="/users/:userId" component={User}/> */}
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/NeighborhoodsPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+class NeighborhoodsPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I'm the Page of Neighborhoods</h1>
+                <h2>Candler Park</h2>
+                <h2>Little Five Points</h2>
+                <h2>Old Fourth Ward</h2>
+            </div>
+        )
+    }
+}
+
+export default NeighborhoodsPage
