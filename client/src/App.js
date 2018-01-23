b7b2abac73f12ed2842cfec74fdbf95fee88c479
tonycimaglia <tonycimaglia@gmail.com>
2018-01-23 10:18:20 -0500

    Neighborhoods Componenet loads when the app loads

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import styled from 'styled-components'
-import NeighborhoodsPage from './components/NeighborhoodsPage'
+import NeighborhoodsPage from './components/banana'
 // import User from './components/User'
 // import UsersList from './components/UsersList'
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={NeighborhoodsPage}/>
-            {/* <Route path="/users" component={UsersList}/>
-            <Route path="/users/:userId" component={User}/> */}
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <NeighborhoodsPage />
+      </div>
+      // <Router>
+      //   <div>
+      //     <Switch>
+      //       {/* <Route exact path="/" component={NeighborhoodsPage} /> */}
+      //       {/* <Route path="/users" component={UsersList}/>
+      //       <Route path="/users/:userId" component={User}/> */}
+      //     </Switch>
+      //   </div>
+      // </Router>
     )
   }
 }
