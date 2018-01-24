02623dcfbbf756c57671222cabd8f816f5003cac
tonycimaglia <tonycimaglia@gmail.com>
2018-01-24 10:00:58 -0500

    added an update route in my users controller.

--- controllers/users.js
+++ controllers/users.js
@@ -23,8 +23,22 @@ router.post('/', (req, res) => {
 
 router.delete('/:userId', async (req, res) => {
   try {
-    await User.findByIdAndRemove(req.params.userId) // Delete the idea from the database
+    await User.findByIdAndRemove(req.params.userId) // Delete the user from the database
     res.sendStatus(200) // Send back a status of 200 to tell the client that the delete was successful
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500) // If there is any error, tell the client something went wrong on the server
+  }
+})
+
+router.patch('/:userId', async (req, res) => {
+  try {
+
+    // Update the user in the database
+    const updatedUser =
+      await User.findByIdAndUpdate(req.params.userId, req.body, {new: true})
+
+    res.json(updatedUser) // Send the updated user back to the client
   } catch (error) {
     console.log(error)
     res.sendStatus(500) // If there is any error, tell the client something went wrong on the server
