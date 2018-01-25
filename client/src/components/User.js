475033e11b2242f4cb0dacb3153d459d9f202468
tonycimaglia <tonycimaglia@gmail.com>
2018-01-25 11:18:57 -0500

    trying to pass down my user info to the user profile page

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import UserProfilePage from './UserProfilePage'
 
 const User = (props) => {
     const url = `/users/${props.user._id}`
     return (
         <div>
+            <UserProfilePage user={props.user} key={props.user._id} />
             <Link to={url}><p>{props.user.userName}</p></Link>
 
             <input type="text"
--- client/src/components/UserProfilePage.js
+++ client/src/components/UserProfilePage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import axios from 'axios'
 
 // get request axios
 // maatch.props.params
@@ -9,6 +8,7 @@ const UserProfilePage = (props) => {
     return (
         <div>
             <p>I'm the user profile page</p>
+            {props.userName}
         </div>
     )
 }
