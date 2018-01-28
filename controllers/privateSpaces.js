cff32b4b989be7a27085922633ccfb470d7d1865
tonycimaglia <tonycimaglia@gmail.com>
2018-01-28 13:01:15 -0500

    refactored private space controller.

--- controllers/privateSpaces.js
+++ controllers/privateSpaces.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const User = require('../db/models/User')
 // const PrivateSpace = require('../db/models/PrivateSpace')
 
-router.get('/:userId', (req, res) => {
-    User.findById(req.params.userId)
-        .then((user) => res.json(user.privateSpace.id(req.params.privateSpaceId)))
-        .catch((error) => { console.log(error) })
+router.get('/', (req, res) => {
+    const userId = req.params.userId
+    User.findById(userId)
+    .then((user) => {
+        res.json(user.privateSpaces)
+    })
+    .catch((error) => console.log(error))
 })
 
 module.exports = router
